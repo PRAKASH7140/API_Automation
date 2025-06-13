b52d16f3ed0d5de55bb93f1e94ec7f129476c907
PRAKASH7140 <prakash25042017@gmail.com>
2025-06-13 12:33:09 +0530

    chage login id

--- RestAssuredTest/src/test/java/com/api/testing/RestAssuredTest/AppTest.java
+++ RestAssuredTest/src/test/java/com/api/testing/RestAssuredTest/AppTest.java
@@ -39,7 +39,7 @@ public class AppTest extends BaseTest {
     @Test(priority = 1)
     public void testUserSignup() {
         JSONObject request = new JSONObject();
-        request.put("email", "dummy2@example.com");
+        request.put("email", "dummy3@example.com");
         request.put("password", "dummy123");
 
         Response response = given()
